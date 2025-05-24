673203fe86d55e334845ae232ae2933409c7c7ee
TechMidia <Techmidia.agencia@gmail.com>
2025-05-23 23:17:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,24 +4,26 @@ import os
 from datetime import datetime
 from pymongo import MongoClient
 import boto3
-from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-# Conexão com MongoDB
+# MongoDB
 MONGO_URI = os.environ.get('MONGO_URI')
 client = MongoClient(MONGO_URI)
 db = client['encartes']
 colecao_produtos = db['produtos_imagem']
 
-# Conexão com Amazon S3
-s3 = boto3.client(
-    's3',
-    aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
-    aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
-    region_name=os.environ.get('AWS_REGION')
+# AWS S3
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_S3_BUCKET = os.environ.get('AWS_S3_BUCKET')
+AWS_REGION = os.environ.get('AWS_REGION')
+
+s3 = boto3.client('s3',
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name=AWS_REGION
 )
-bucket_name = os.environ.get('AWS_S3_BUCKET')
 
 @app.route('/')
 def home():
@@ -48,34 +50,24 @@ def remover_fundo():
     if response.status_code != 200:
         return {'error': 'Erro na API Dezgo', 'status': response.status_code}, 500
 
-    # Nome seguro para o arquivo
-    timestamp = int(datetime.now().timestamp())
-    nome_arquivo = secure_filename(f'{nome_produto}_{timestamp}.png')
-
-    # Salva temporariamente a imagem
+    # Salva imagem local
+    nome_arquivo = f'imagem_sem_fundo_{datetime.now().timestamp()}.png'
     with open(nome_arquivo, 'wb') as f:
         f.write(response.content)
 
-    # Faz o upload para o S3
-    s3.upload_file(nome_arquivo, bucket_name, nome_arquivo)
-    url_s3 = f"https://{bucket_name}.s3.amazonaws.com/{nome_arquivo}"
+    # Envia para o S3
+    s3.upload_file(nome_arquivo, AWS_S3_BUCKET, nome_arquivo)
+    s3_url = f'https://{AWS_S3_BUCKET}.s3.{AWS_REGION}.amazonaws.com/{nome_arquivo}'
 
     # Salva no MongoDB
     produto = {
         'nome': nome_produto,
-        'tipo': 'desconhecido',
-        'imagem_url': 'imagem_original_nao_salva',
-        'imagem_fundo_removido_url': url_s3,
-        'origem': 'upload',
+        'imagem_fundo_removido_url': s3_url,
         'criado_em': datetime.now()
     }
-
     colecao_produtos.insert_one(produto)
 
-    # Remove imagem local
-    os.remove(nome_arquivo)
-
-    return {'mensagem': 'Upload feito com sucesso', 'url_imagem_s3': url_s3}
+    return send_file(nome_arquivo, mimetype='image/png')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
