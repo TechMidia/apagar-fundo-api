55895b8c79bcd64462b230bae59781f5779312fc
TechMidia <Techmidia.agencia@gmail.com>
2025-05-23 23:39:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -78,4 +78,9 @@ def remover_fundo():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port, debug=False, use_reloader=False)
+    app.run(
+        host='0.0.0.0',
+        port=port,
+        debug=False,
+        use_reloader=False
+    )
