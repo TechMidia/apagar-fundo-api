1ae5494aeab74a59802c12c46650069131d46a35
TechMidia <Techmidia.agencia@gmail.com>
2025-05-23 23:19:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -70,5 +70,5 @@ def remover_fundo():
     return send_file(nome_arquivo, mimetype='image/png')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))  # use 8080 como padrão
     app.run(host='0.0.0.0', port=port)
